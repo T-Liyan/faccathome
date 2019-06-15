534887801b456dd3b0bf79f10d9049a14f3ab691
丽燕 谭 <babyliyan@live.com>
2019-06-15 20:47:04 +0800

    init

---
+++ src/FatCatHomeReact/src/App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      <div className="Content">
+        <div className="Content1">
+          <img src="./images/catone.jpg" alt="catone"></img>
+        </div>
+        <div className="Content2">
+          <div className="Content3">
+            <p>肥猫刷牙肥猫刷牙肥猫刷牙肥猫刷牙肥猫刷牙肥猫刷牙肥猫刷牙肥猫刷牙肥猫刷牙肥猫刷牙肥猫刷牙</p>
+          </div>
+            <p>17:01</p>
+            <p>23评论</p>
+
+        </div>
+      </div>
+
+      <div className="Content">
+        <div className="Content1">
+          <img src="./images/catthree.jpg" alt="catthree"></img>
+        </div>
+        <div className="Content2">
+          <div className="Content3">
+            <p>肥猫蹦蹦肥猫蹦蹦肥猫蹦蹦肥猫蹦蹦肥猫蹦蹦肥猫蹦蹦肥猫蹦蹦肥猫蹦蹦肥猫蹦蹦</p>
+          </div>
+            <p>17:24</p>
+            <p>10评论</p>
+        </div>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
